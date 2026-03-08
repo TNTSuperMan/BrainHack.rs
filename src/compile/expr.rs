529872eccfd10d988e9b6d877e5bc0d8a80d15d0
TNTSuperMan <111977067+TNTSuperMan@users.noreply.github.com>
2026-03-08 17:47:11 +0900

    expression

--- src/compile/expr.rs
+++ src/compile/expr.rs
@@ -8,8 +8,40 @@ use crate::{asm::asm::AssemblyOp, compile::ctx::CompileContext, ir::ir::{IRExpr,
 pub fn compile_expr(ctx: &mut CompileContext, funcs: &HashMap<Identifier, IRFunc>, target: usize, expr: &IRExpr) -> Result<Vec<AssemblyOp>> {
     let mut asm: Vec<AssemblyOp> = vec![];
     ctx.push();
-    // TODO
-    bail!("unimplemented");
+    
+    match expr {
+        IRExpr::Const(c) => {
+            asm.push(AssemblyOp::Set(target, *c));
+        }
+        IRExpr::Add(l, r) => {
+            asm.append(&mut compile_expr(ctx, funcs, target, l.as_ref())?);
+            
+            let rp = ctx.alloc_noname();
+            asm.append(&mut compile_expr(ctx, funcs, rp, r.as_ref())?);
+
+            asm.push(AssemblyOp::Move(rp, vec![(target, 1)]));
+            ctx.free(rp)?;
+        }
+        IRExpr::Sub(l, r) => {
+            asm.append(&mut compile_expr(ctx, funcs, target, l.as_ref())?);
+            
+            let rp = ctx.alloc_noname();
+            asm.append(&mut compile_expr(ctx, funcs, rp, r.as_ref())?);
+
+            asm.push(AssemblyOp::Move(rp, vec![(target, -1)]));
+            ctx.free(rp)?;
+        }
+
+        IRExpr::Id { id, last_use: _ } => {
+            let ptr = ctx.get(*id)?;
+            let tmp = ctx.alloc_noname();
+            asm.push(AssemblyOp::Move(ptr, vec![(tmp, 1)]));
+            asm.push(AssemblyOp::Move(tmp, vec![(ptr, 1), (target, 1)]));
+            ctx.free(tmp)?;
+        }
+
+        _ => bail!("todo")
+    }
     
     ctx.pop();
     Ok(asm)
