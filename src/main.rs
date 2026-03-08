005c86d18174e3aa450ac044d81b6155900b95a0
TNTSuperMan <111977067+TNTSuperMan@users.noreply.github.com>
2026-03-08 16:36:06 +0900

    CLIの形を作る

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,31 @@ mod asm;
 mod ir;
 mod compile;
 
-use std::path::Path;
+use std::{env::args, fs, path::Path, process::ExitCode};
 
-use crate::{asm::asm::{AssemblyOp, AssemblyProgram}, ir::parse_to_ir};
+use anyhow::Result;
 
-fn main() {
-    println!("{:?}", parse_to_ir(&Path::new("./box/example.js")));
+use crate::{compile::compile, ir::parse_to_ir};
+
+fn resulty_main(input: &str, output: &str) -> Result<()> {
+    let ir = parse_to_ir(&Path::new(input))?;
+    let asm = compile(&ir)?;
+    fs::write(output, asm.assemble())?;
+    Ok(())
+}
+
+fn main() -> ExitCode {
+    let args: Vec<String> = args().collect();
+    
+    if args.len() < 3 {
+        println!("usage: {} [INPUT] [OUTPUT]", args[0]);
+        ExitCode::FAILURE
+    } else {
+        if let Err(e) = resulty_main(&args[1], &args[2]) {
+            eprintln!("Error: {e:?}");
+            ExitCode::FAILURE
+        } else {
+            ExitCode::SUCCESS
+        }
+    }
 }
