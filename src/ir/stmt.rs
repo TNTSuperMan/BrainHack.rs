e4ac395e4bfe7b4e46c7b5ebcc498eacf51cc174
TNTSuperMan <111977067+TNTSuperMan@users.noreply.github.com>
2026-03-08 15:20:43 +0900

    parse_stmtの引数をStatemetListItemからStatementにする

--- src/ir/stmt.rs
+++ src/ir/stmt.rs
@@ -5,132 +5,133 @@ use crate::ir::{expr::parse_expr, ir::{IRExpr, IRStmt, IRVarInit}};
 
 fn parse_block(statement: &Statement) -> Result<Vec<IRStmt>> {
     if let Statement::Block(block) = statement {
-        block.statement_list().iter().map(parse_stmt).collect()
+        block.statement_list().iter().map(|s| {
+            if let StatementListItem::Statement(stmt) = s {
+                parse_stmt(&stmt)
+            } else {
+                bail!("unsupport");
+            }
+        }).collect()
     } else {
-        parse_stmt(&StatementListItem::Statement(Box::new(statement.clone()))).map(|stmt| {
+        parse_stmt(statement).map(|stmt| {
             vec![stmt]
         })
     }
 }
 
-pub fn parse_stmt(statement: &StatementListItem) -> Result<IRStmt> {
+pub fn parse_stmt(statement: &Statement) -> Result<IRStmt> {
     match statement {
-        StatementListItem::Statement(stmt) => {
-            match stmt.as_ref() {
-                Statement::Var(var) => {
-                    var.0.as_ref().iter().map(|d| {
-                        if let Binding::Identifier(id) = d.binding() {
-                            Ok(IRVarInit {
-                                id: *id,
-                                init: if let Some(i) = d.init() {
-                                    Some(parse_expr(i)?)
-                                } else {
-                                    None
-                                },
-                            })
-                        } else {
-                            bail!("unsupport");
-                        }
-                    }).collect::<Result<Vec<IRVarInit>>>().map(|vars| {
-                        IRStmt::VariableDefine { vars }
-                    })
-                }
-                Statement::Expression(expr) => {
-                    match expr {
-                        Expression::Assign(assign) => {
-                            if let AssignTarget::Identifier(id) = assign.lhs() {
-                                let val = parse_expr(assign.rhs())?;
-                                Ok(match assign.op() {
-                                    AssignOp::Assign => IRStmt::Assign { id: *id, value: val },
-                                    AssignOp::Add => IRStmt::Assign { id: *id, value: IRExpr::Add(Box::new(IRExpr::Id(*id)), Box::new(val)) },
-                                    AssignOp::Sub => IRStmt::Assign { id: *id, value: IRExpr::Sub(Box::new(IRExpr::Id(*id)), Box::new(val)) },
-                                    AssignOp::Mul => IRStmt::Assign { id: *id, value: IRExpr::Mul(Box::new(IRExpr::Id(*id)), Box::new(val)) },
-                                    AssignOp::Div => IRStmt::Assign { id: *id, value: IRExpr::Div(Box::new(IRExpr::Id(*id)), Box::new(val)) },
-                                    _ => bail!("unsupport"),
-                                })
-                            } else {
-                                bail!("unsupport");
-                            }
-                        }
-                        Expression::Call(call) => {
-                            if let Expression::Identifier(id) = call.function() {
-                                call.args().iter().map(|e| parse_expr(e)).collect::<Result<Vec<IRExpr>>>().map(|args| {
-                                    IRStmt::Call {
-                                        id: *id,
-                                        args,
-                                    }
-                                })
-                            } else {
-                                bail!("unsupport");
-                            }
-                        }
-                        Expression::Update(upd) => {
-                            if let UpdateTarget::Identifier(id) = upd.target() {
-                                let id_expr = Box::new(IRExpr::Id(*id));
-                                let one_expr = Box::new(IRExpr::Const(1));
-                                Ok(match upd.op() {
-                                    UpdateOp::IncrementPost |
-                                    UpdateOp::IncrementPre => IRStmt::Assign { id: *id, value: IRExpr::Add(id_expr, one_expr) },
-                                    UpdateOp::DecrementPost |
-                                    UpdateOp::DecrementPre => IRStmt::Assign { id: *id, value: IRExpr::Sub(id_expr, one_expr) },
-                                })
-                            } else {
-                                bail!("unsupport");
-                            }
-                        }
-                        _ => bail!("unimp | unsupport"),
-                    }
-                }
-                Statement::WhileLoop(whileloop) => {
-                    Ok(IRStmt::While {
-                        condition: parse_expr(whileloop.condition())?,
-                        body: parse_block(whileloop.body())?,
-                    })
-                }
-                Statement::If(if_ast) => {
-                    Ok(IRStmt::If {
-                        condition: parse_expr(if_ast.cond())?,
-                        body: parse_block(if_ast.body())?,
-                        else_body: if let Some(s) = if_ast.else_node() {
-                            Some(parse_block(s)?)
+        Statement::Var(var) => {
+            var.0.as_ref().iter().map(|d| {
+                if let Binding::Identifier(id) = d.binding() {
+                    Ok(IRVarInit {
+                        id: *id,
+                        init: if let Some(i) = d.init() {
+                            Some(parse_expr(i)?)
                         } else {
                             None
                         },
                     })
+                } else {
+                    bail!("unsupport");
                 }
-                Statement::Block(_) => {
-                    Ok(IRStmt::Block { body: parse_block(stmt)? })
-                }
-                Statement::ForLoop(for_ast) => {
-                    let mut body: Vec<IRStmt> = vec![];
-
-                    if let Some(init) = for_ast.init() {
-                        body.push(match init {
-                            ForLoopInitializer::Var(var) => parse_stmt(&StatementListItem::Statement(Box::new(Statement::Var(var.clone()))))?,
-                            ForLoopInitializer::Expression(expr) => parse_stmt(&StatementListItem::Statement(Box::new(Statement::Expression(expr.clone()))))?,
+            }).collect::<Result<Vec<IRVarInit>>>().map(|vars| {
+                IRStmt::VariableDefine { vars }
+            })
+        }
+        Statement::Expression(expr) => {
+            match expr {
+                Expression::Assign(assign) => {
+                    if let AssignTarget::Identifier(id) = assign.lhs() {
+                        let val = parse_expr(assign.rhs())?;
+                        Ok(match assign.op() {
+                            AssignOp::Assign => IRStmt::Assign { id: *id, value: val },
+                            AssignOp::Add => IRStmt::Assign { id: *id, value: IRExpr::Add(Box::new(IRExpr::Id(*id)), Box::new(val)) },
+                            AssignOp::Sub => IRStmt::Assign { id: *id, value: IRExpr::Sub(Box::new(IRExpr::Id(*id)), Box::new(val)) },
+                            AssignOp::Mul => IRStmt::Assign { id: *id, value: IRExpr::Mul(Box::new(IRExpr::Id(*id)), Box::new(val)) },
+                            AssignOp::Div => IRStmt::Assign { id: *id, value: IRExpr::Div(Box::new(IRExpr::Id(*id)), Box::new(val)) },
                             _ => bail!("unsupport"),
-                        });
+                        })
+                    } else {
+                        bail!("unsupport");
                     }
-
-                    let mut loop_body = parse_block(for_ast.body())?;
-
-                    if let Some(upd) = for_ast.final_expr() {
-                        loop_body.push(parse_stmt(&StatementListItem::Statement(Box::new(Statement::Expression(upd.clone()))))?);
-                    }
-
-                    body.push(IRStmt::While {
-                        condition: for_ast.condition().map_or(
-                            Ok(IRExpr::Const(1)),
-                            parse_expr
-                        )?,
-                        body: loop_body,
-                    });
-
-                    Ok(IRStmt::Block { body })
                 }
-                _ => bail!("unimp")
+                Expression::Call(call) => {
+                    if let Expression::Identifier(id) = call.function() {
+                        call.args().iter().map(|e| parse_expr(e)).collect::<Result<Vec<IRExpr>>>().map(|args| {
+                            IRStmt::Call {
+                                id: *id,
+                                args,
+                            }
+                        })
+                    } else {
+                        bail!("unsupport");
+                    }
+                }
+                Expression::Update(upd) => {
+                    if let UpdateTarget::Identifier(id) = upd.target() {
+                        let id_expr = Box::new(IRExpr::Id(*id));
+                        let one_expr = Box::new(IRExpr::Const(1));
+                        Ok(match upd.op() {
+                            UpdateOp::IncrementPost |
+                            UpdateOp::IncrementPre => IRStmt::Assign { id: *id, value: IRExpr::Add(id_expr, one_expr) },
+                            UpdateOp::DecrementPost |
+                            UpdateOp::DecrementPre => IRStmt::Assign { id: *id, value: IRExpr::Sub(id_expr, one_expr) },
+                        })
+                    } else {
+                        bail!("unsupport");
+                    }
+                }
+                _ => bail!("unimp | unsupport"),
             }
         }
-        _ => bail!("err"),
+        Statement::WhileLoop(whileloop) => {
+            Ok(IRStmt::While {
+                condition: parse_expr(whileloop.condition())?,
+                body: parse_block(whileloop.body())?,
+            })
+        }
+        Statement::If(if_ast) => {
+            Ok(IRStmt::If {
+                condition: parse_expr(if_ast.cond())?,
+                body: parse_block(if_ast.body())?,
+                else_body: if let Some(s) = if_ast.else_node() {
+                    Some(parse_block(s)?)
+                } else {
+                    None
+                },
+            })
+        }
+        Statement::Block(_) => {
+            Ok(IRStmt::Block { body: parse_block(statement)? })
+        }
+        Statement::ForLoop(for_ast) => {
+            let mut body: Vec<IRStmt> = vec![];
+
+            if let Some(init) = for_ast.init() {
+                body.push(match init {
+                    ForLoopInitializer::Var(var) => parse_stmt(&Statement::Var(var.clone()))?,
+                    ForLoopInitializer::Expression(expr) => parse_stmt(&Statement::Expression(expr.clone()))?,
+                    _ => bail!("unsupport"),
+                });
+            }
+
+            let mut loop_body = parse_block(for_ast.body())?;
+
+            if let Some(upd) = for_ast.final_expr() {
+                loop_body.push(parse_stmt(&Statement::Expression(upd.clone()))?);
+            }
+
+            body.push(IRStmt::While {
+                condition: for_ast.condition().map_or(
+                    Ok(IRExpr::Const(1)),
+                    parse_expr
+                )?,
+                body: loop_body,
+            });
+
+            Ok(IRStmt::Block { body })
+        }
+        _ => bail!("unimp")
     }
 }
